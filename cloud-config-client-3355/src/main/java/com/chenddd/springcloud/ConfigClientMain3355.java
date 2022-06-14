0fef75adc7ec857ed01472bca2bf431140e94da8
chenddd <2690541998@qq.com>
2022-06-14 19:19:34 +0800

    config

---
+++ cloud-config-client-3355/src/main/java/com/chenddd/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.chenddd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Author: chenddd
+ * Date: 2022/6/14 16:54
+ * FileName: ConfigClientMain3355
+ * Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
