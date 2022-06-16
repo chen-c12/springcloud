13c8b43ac3e41a0b255942bfc628e5a7e5105250
chenddd <2690541998@qq.com>
2022-06-16 14:43:54 +0800

    spring-cloud-bus

---
+++ cloud-config-client-3366/src/main/java/com/chenddd/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.chenddd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Author: chenddd
+ * Date: 2022/6/15 14:01
+ * FileName: ConfigClientMain3366
+ * Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/chenddd/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.chenddd.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: chenddd
+ * Date: 2022/6/16 14:11
+ * FileName: ConfigClientController
+ * Description:
+ */
+@RefreshScope
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
